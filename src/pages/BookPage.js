17cade61f10a99dac6a8e5907aaba474fc226f0b
tanuj7077 <piku.tanuj@gmail.com>
2021-09-08 23:57:27 +0530

    bookPage top section ui complete

--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { FaPlay } from "react-icons/fa";
+import { BiStats } from "react-icons/bi";
 import { MdCollectionsBookmark } from "react-icons/md";
 import { FaStar, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Chapters from "./BookPage/Chapters";
 import Summary from "./BookPage/Summary";
 import Reviews from "./BookPage/Reviews";
+import bg1 from "../assets/bg-3.jpg";
 
 const BookPage = () => {
   const [selection, setSelection] = useState("summary");
   let novelObj = {
-    img: "https://mangageko.com/media/manga_covers/cover_1_DrJkaWz.jpg",
+    img: "https://mangageko.com/media/manga_covers/trickster.jpeg",
     title: "Reincarnation of the Strongest Swordsman in Universe",
     author: "Kim Yohan",
     chapters: 145,
@@ -48,7 +50,119 @@ const BookPage = () => {
   ];
   return (
     <div className="bookPage">
-      <section className="introSection">
+      <div className="bookPage-container">
+        <div className="introSection">
+          <span
+            className="introSection-bgImg"
+            style={{
+              backgroundImage: `url(${bg1})`,
+            }}
+          ></span>
+          <div className="introSection-container">
+            <span
+              className="introSection-container-img"
+              style={{
+                backgroundImage: `url(${novelObj.img})`,
+              }}
+            ></span>
+            <div className="introSection-container-content">
+              <div className="introSection-container-content-titleSection">
+                <p className="name">
+                  <span className="novelName">{novelObj.title}</span>
+                  <span className="authorName">{novelObj.author}</span>
+                </p>
+                <div className="ratingSection">
+                  <div className="rating">{novelObj.rating.rating}</div>
+                  <FaStar className="icon" />
+                </div>
+              </div>
+              <div className="introSection-container-content-midSection">
+                <div className="introSection-container-content-infoSectionCard">
+                  <div className="subheading">Stats</div>
+                  <div className="introSection-container-content-infoSection">
+                    <div className="introSection-container-content-infoSection-item">
+                      <div className="count">{novelObj.chapters}</div>
+                      <div className="subheading">Chapters</div>
+                    </div>
+                    <div className="introSection-container-content-infoSection-item">
+                      <div className="count">{novelObj.rank}</div>
+                      <div className="subheading">Rank</div>
+                    </div>
+                    {/* <div className="introSection-container-content-infoSection-item">
+                    <div className="subheading">Status</div>
+                    <div className="count">{novelObj.status}</div>
+                  </div> */}
+                    <div className="introSection-container-content-infoSection-item">
+                      <div className="count">{novelObj.bookmarked}</div>
+                      <div className="subheading">Bookmarked</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="introSection-container-content-otherSectionCard">
+                  <div className="subheading">Categories</div>
+                  <div className="introSection-container-content-categorySection">
+                    {/* <div className="subheading">Categories</div> */}
+                    <div className="tags">
+                      {novelObj.tags.map((tag) => {
+                        return <p className="tags-tag">{tag}</p>;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="introSection-container-content-buttons">
+                <div className="add btn">
+                  <span className="text">Add to Collection</span>
+                </div>
+                <div className="start btn">
+                  <span className="text">{novelObj.readStatus}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section className="selection">
+          <div
+            className={`item ${
+              selection === "summary" ? "currentSelection" : ""
+            }`}
+            onClick={() => {
+              setSelection("summary");
+            }}
+          >
+            Summary
+          </div>
+          <div
+            className={`item ${
+              selection === "chapters" ? "currentSelection" : ""
+            }`}
+            onClick={() => {
+              setSelection("chapters");
+            }}
+          >
+            Chapters
+          </div>
+          <div
+            className={`item ${
+              selection === "reviews" ? "currentSelection" : ""
+            }`}
+            onClick={() => {
+              setSelection("reviews");
+            }}
+          >
+            Reviews
+          </div>
+        </section>
+
+        {selection === "summary" && <Summary />}
+        {selection === "chapters" && <Chapters allChapters={allChapters} />}
+        {selection === "reviews" && <Reviews />}
+        <section className="commentSection"></section>
+        <section className="recommendedSection"></section>
+      </div>
+
+      {/* <section className="introSection">
         <div className="introSection-img">
           <div
             className="image"
@@ -100,54 +214,15 @@ const BookPage = () => {
           </div>
           <div className="introSection-info-buttons">
             <div className="add btn">
-              {/* <MdCollectionsBookmark className="icon" /> */}
               <span className="text">Add to Collection</span>
             </div>
             <div className="start btn">
-              {/* <FaPlay className="icon" /> */}
               <span className="text">{novelObj.readStatus}</span>
             </div>
           </div>
         </div>
       </section>
-      <section className="selection">
-        <div
-          className={`item ${
-            selection === "summary" ? "currentSelection" : ""
-          }`}
-          onClick={() => {
-            setSelection("summary");
-          }}
-        >
-          Summary
-        </div>
-        <div
-          className={`item ${
-            selection === "chapters" ? "currentSelection" : ""
-          }`}
-          onClick={() => {
-            setSelection("chapters");
-          }}
-        >
-          Chapters
-        </div>
-        <div
-          className={`item ${
-            selection === "reviews" ? "currentSelection" : ""
-          }`}
-          onClick={() => {
-            setSelection("reviews");
-          }}
-        >
-          Reviews
-        </div>
-      </section>
-
-      {selection === "summary" && <Summary />}
-      {selection === "chapters" && <Chapters allChapters={allChapters} />}
-      {selection === "reviews" && <Reviews />}
-      <section className="commentSection"></section>
-      <section className="recommendedSection"></section>
+       */}
     </div>
   );
 };
