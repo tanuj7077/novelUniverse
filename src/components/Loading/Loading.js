c30a9a263d6cf95cad2d0440a7afca9c64169c0c
tanuj7077 <piku.tanuj@gmail.com>
2021-11-28 23:26:28 +0530

    landing page loading animation removal

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useGlobalContext } from "../../context";
+import { useLocation } from "react-router-dom";
 
 const Loading = () => {
   const { isLoading } = useGlobalContext();
-  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(isLoading);
-    console.log(isLoading);
   }, [isLoading]);
+  useEffect(() => {
+    let arr = location.pathname.split("/").map(String);
+    if (arr.length > 1) {
+      //console.log(arr[1]);
+      console.log(arr[1] === "");
+    }
+  }, [location]);
   return (
-    <div className={`loadingAnim ${!loading && "loadingAnim-invisible"}`}></div>
+    <>
+      <div
+        className={`loadingAnim ${
+          (!loading || location.pathname.split("/").map(String)[1] === "") &&
+          "loadingAnim-invisible"
+        }`}
+      ></div>
+    </>
   );
 };
 
